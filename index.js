1351e44c78405e84368cb16a2886ae20b31dcf19
Marcos Koch <marcoskoch@MacBook-Pro-de-Marcos.local>
2018-10-18 11:11:44 -0300

    fix: eslint recommendation

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-class Message{
+class Message {
   constructor(text = '', created = Date.now()) {
     this.text = text;
     this.created = created;
   }
+
   toString() {
     return `Message created at: ${this.created} - Text: ${this.text}`;
   }
@@ -10,21 +11,36 @@ class Message{
 
 class ImageMassage extends Message {
   constructor(text = '', created = Date.now(),
-              url = '', thumbnail = '') {
+    url = '', thumbnail = '') {
     super(text, created);
     this.url = url;
     this.thumbnail = thumbnail;
   }
+
+  get created() {
+    return this._created;
+  }
+
+  set created(created) {
+    if (typeof created === 'undefined' || isNaN(created)) {
+      throw new Error('Invalid created');
+    }
+    if (Message.prototype.hasOwnProperty.call(this, '_created')) {
+      throw new Error('Created already defined');
+    }
+    this._created = created;
+  }
+
   toString() {
-    return `Photo${super.toString()} ` +
-           `- Url: ${this.url} ` +
-           `Thumbnail: ${this.thumbnail}`;
+    return `Photo${super.toString()} `
+           + `- Url: ${this.url} `
+           + `Thumbnail: ${this.thumbnail}`;
   }
 }
 
-var emptyMessage = new Message();
-var textMessage = new Message('yesterday massage', Date.now() - 86400);
-var imageMessage = new ImageMassage();
+const emptyMessage = new Message();
+const textMessage = new Message('yesterday massage', Date.now() - 86400);
+const imageMessage = new ImageMassage();
 
 
 console.log(String(emptyMessage));
